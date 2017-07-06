05ec37feac08493b9ac725f8bb8d4da58aec1f49
yanchuanqi <ycqstrong@icloud.com>
2017-07-06 10:27:58 +0800

    。。

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@
 #include <list>
 #include <regex>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 int main() {
     string name;
     string doc,tmplink,tmptit,tmppass;
-    string pattern("http(s)?://([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?");
-    pattern = "[[:alpha:]]*" + pattern + "[[:alpha:]]*";
+//    string pattern("http(s)?://([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?");
+//    pattern = "[[:alpha:]]*" + pattern + "[[:alpha:]]*";
     ifstream infile;
     ofstream outfile("output.txt");
     list<string> link;
@@ -22,9 +23,11 @@ int main() {
     unsigned long pos1,pos2;
     unsigned long p1,p2;
     int count=0;
+    string pattern("http(s)?://([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?");
+//    pattern = "[[:alpha:]]*" + pattern + "[[:alpha:]]*";
+    regex r(pattern);
 
-
-    name="/Users/yanchuanqi/ClionProjects/filetest/HDU题目分类.webarchive";
+    name="/Users/yanchuanqi/ClionProjects/filetest/中国人民大学信息学院.webarchive";
     infile.open(name);
     while (getline(infile,doc)) {
         if(doc.find("<!--")!=-1) continue;
@@ -55,54 +58,92 @@ int main() {
                     tmppass.erase(p1,p2-p1+1);
                 }
             }
-            if(tmppass=="&nbsp;") continue;
             p1=tmppass.find_first_of('<');
             p2=tmppass.find_first_of('>',p1+1);
             if(p1 != -1&&p2 != -1) {
                 tmppass.erase(p1,p2-p1+1);
             }
+            //删除空格
+            p1=tmppass.find("&nbsp;");
+            while (p1 != -1) {
+                tmppass.erase(p1,6);
+                p1=tmppass.find("&nbsp;");
+            }
+            //删除完毕
+            if(tmppass.length()==0) continue;
             passage.push_back(tmppass);
             count++;
         }
         //内容完
 
 
-        //超链接
-        pos1=doc.find("href=\"");
-        pos2=doc.find_first_of("\"",pos1 + 6);
-        if(pos1 != -1) {
-            tmplink=doc.substr(pos1 + 6,pos2 - pos1 - 6);
-            link.push_back(tmplink);
-            count++;
-        }
-        pos1=doc.find("src=\"");
-        pos2=doc.find_first_of("\"",pos1 + 5);
-        if(pos1 != -1) {
-            tmplink=doc.substr(pos1 + 5,pos2 - pos1 - 5);
-            link.push_back(tmplink);
-        }
-        //超链接完
+//        //超链接
+
+//        for (sregex_iterator it(doc.begin(), doc.end(), r), end;     //end是尾后迭代器，regex_iterator是regex_iterator的string类型的版本
+//             it != end;
+//             ++it)
+//        {
+//            cout << it->str() << endl;
+//        }
+//        pos1=doc.find("href=\"");
+//        pos2=doc.find_first_of("\"",pos1 + 6);
+//        if(pos1 != -1) {
+//            tmplink=doc.substr(pos1 + 6,pos2 - pos1 - 6);
+//            //加http：
+//            while (tmplink[0]=='/') {
+//                tmplink.erase(0);
+//            }
+//            if(tmplink.length()==0) continue;
+//            if(tmplink.find("http")==-1) tmplink.insert(0,"http://");
+//            //加完了
+//            link.push_back(tmplink);
+//        }
+//        pos1=doc.find("src=\"");
+//        pos2=doc.find_first_of("\"",pos1 + 5);
+//        if(pos1 != -1) {
+//            tmplink=doc.substr(pos1 + 5,pos2 - pos1 - 5);
+//            //加http：
+//            while (tmplink[0]=='/') {
+//                tmplink.erase(0);
+//            }
+//            if(tmplink.length()==0) continue;
+//            if(tmplink.find("http")==-1) tmplink.insert(0,"http://");
+//            //加完了
+//            link.push_back(tmplink);
+//        }
+//        //超链接完
     }
+
+
     outfile<<"标题："<<endl;
+    cout<<"标题："<<endl;
     for (titleIterator = title.begin(); titleIterator != title.end() ; titleIterator++) {
         outfile<<*titleIterator<<endl;
+        cout<<*titleIterator<<endl;
 
     }
     outfile<<endl<<endl;
+    cout<<endl<<endl;
 
     outfile<<"正文："<<endl;
+    cout<<"正文"<<endl;
     for (passageIterator = passage.begin(); passageIterator != passage.end() ; passageIterator++) {
         outfile<<*passageIterator<<endl;
+        cout<<*passageIterator<<endl;
     }
     outfile<<endl<<endl;
+    cout<<endl<<endl;
 
     link.sort();
     link.unique();
     outfile<<"超链接地址："<<endl;
+    cout<<"超链接地址："<<endl;
     for (linkIterator = link.begin(); linkIterator != link.end() ; linkIterator++) {
         outfile<<*linkIterator<<endl;
+        cout<<*linkIterator<<endl;
     }
     outfile<<endl<<endl;
+    cout<<endl<<endl;
 
     infile.close();
 
